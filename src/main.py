bf367ec0495c17e1f15dddce7a346b99f26a68ea
Jonny Le <jonnyle2@gmail.com>
2025-02-11 19:03:23 -0600

    wip: required 3 sp 1st rd and continue rd

--- src/main.py
+++ src/main.py
@@ -2,7 +2,10 @@ from collections import deque
 from dataclasses import dataclass
 from typing import Iterable
 
+from tabulate import tabulate
+
 import rule
+from rule import Single, Pair, Straight, Triple, Quad
 from deck import Card, Rank, Suit, deal_deck
 
 
@@ -16,6 +19,42 @@ class Player:
 class Seat:
     player: Player
     hand: set[Card]
+
+
+def print_and_get_card_selection(seat: Seat, to_beat: Card | None = None) -> Single | Pair | Straight | Triple | Quad | None:
+    hand_str = [f'[{card.rank} {card.suit}]' for card in seat.hand]
+    print(tabulate([hand_str, range(1, len(hand_str)+1)]))  # print hand and indexes
+    while True:
+        if to_beat:
+            print(f'{str(type(to_beat)).capitalize()}s round.')
+            print(f'Current: {to_beat}')
+        else:
+            print('New round. Play any combination to start.')
+        play_input = input(f'{seat.player} - select cards separated by spaces: ').split()
+        try:
+            cards = {seat.hand[int(i)-1] for i in play_input}
+        except ValueError:
+            if to_beat and play_input[0].lower() == 'pass':
+                return None
+            print('Please enter only numbers.')
+            continue
+        except IndexError:
+            print(f'Please enter only numbers from 1-{len(len(seat.hand))}.')
+            continue
+        try:
+            play = rule.get_combination(cards)
+        except ValueError as e:
+            print(str(e))
+            continue
+        if to_beat:
+            if not isinstance(play, type(to_beat)):
+                print(f'Round is {str(type(to_beat)).lower()}s. Please only ')
+                continue
+            if play < to_beat:
+                print(f'Combination must be higher than {str(to_beat)}.')
+                continue
+        seat.hand.difference_update(cards)
+        return play
 
 
 def start_game(players: Iterable[str]):
@@ -37,13 +76,26 @@ def start_game(players: Iterable[str]):
         return  # instant win, game ends
 
     winners = []
-    first = next((i, seat) for i, seat in enumerate(seats) if Card(Rank.THREE, Suit.SPADES) in seat.hand)
+    round_winner_index = next(i for i in range(len(seats)) if Card(Rank.THREE, Suit.SPADES) in seats[i].hand)
     round_order = deque()
-    round_order.extendleft(seats[first[0]:])
-    round_order.extendleft(seats[:first[0]])
+    round_order.extendleft(seats[round_winner_index:])  # append in reverse order so pop() works properly
+    round_order.extendleft(seats[:round_winner_index])
     while len(winners) < 3:
-        while len(round_order > 1):
-        
+        # Start round
+        play = print_and_get_card_selection(round_order[-1])
+        round_order.rotate()
+        while len(round_order) > 1:
+            seat_turn = round_order[-1]
+            play = print_and_get_card_selection(seat_turn, play)
+            if play is None:
+                round_order.pop()
+                continue
+            if len(seat_turn.hand) == 0:
+                seats.remove(seat_turn)
+                winners.append(seat_turn)
+            round_order.rotate()
+        round_winner_index = seats.index(round_order[0])
+        round_order.
 
 
 if __name__ == '__main__':
